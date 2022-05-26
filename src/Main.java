e5031e7073559632d5c9695b08cc8441e48b6244
Denver <den-knyazev@mail.ru>
2022-05-27 00:19:18 +0300

    третий каммит

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        //Задание 1
         byte clientOS = 0;
-        if (clientOS==0) {
+        if (clientOS == 0) {
             System.out.println("Установите версию для IOS по ссылке");
         } else {
             System.out.println("Установите версию для Android по ссылке");
@@ -12,18 +13,18 @@ public class Main {
         ;
         int year = 2014;
 
-        if (clientOS==0 && year<=2015) {
+        if (clientOS == 0 && year <= 2015) {
             System.out.println("Установите облегченную версию для IOS по ссылке");
-        } else if(clientOS==1&&year<=2015) {
+        } else if (clientOS == 1 && year <= 2015) {
             System.out.println("Установите облегченную версию на Android по ссылке");
-        }else if(clientOS==0&&year>=2015) {
+        } else if (clientOS == 0 && year >= 2015) {
             System.out.println("Установите версию для IOS по ссылке");
-        } else if(clientOS==1&&year>=2015){
+        } else if (clientOS == 1 && year >= 2015) {
             System.out.println("Установите версию для Android по ссылке");
         }
         // Задание 3
-        int yearIsNow =2021;
-        if (yearIsNow %4 ==0&&yearIsNow %100 !=0||yearIsNow %400 ==0){
+        int yearIsNow = 2021;
+        if (yearIsNow % 4 == 0 && yearIsNow % 100 != 0 || yearIsNow % 400 == 0) {
             System.out.println("Год являеться високосным");
         } else {
             System.out.println("Год не являеться високосным");
@@ -31,17 +32,17 @@ public class Main {
             //Задание 4
             int distation = 95;
             int days = 0;
-            if (distation<20)
-                days =1;
-            else if (distation>=20&&distation<60)
-                days =2;
-            else if (distation>=60&&distation<100)
-                days= 3;
-            System.out.println("Потребуеться дней :"+days);
+            if (distation < 20)
+                days = 1;
+            else if (distation >= 20 && distation < 60)
+                days = 2;
+            else if (distation >= 60 && distation < 100)
+                days = 3;
+            System.out.println("Потребуеться дней :" + days);
             //Задание 5
 
             int mountNomber = 12;
-            switch (mountNomber){
+            switch (mountNomber) {
                 case 12:
                     System.out.println("Зима");
                     break;
